6c6b20493180cfde9e2e89f0b40d80b4c45032e4
Robert Wolfinger <rwolfinger@cellular.de>
2015-09-07 20:39:47 +0200

    JUnit XmlMapping Test

--- core/src/test/java/slim/core/XmlMappingTest.java
+++ core/src/test/java/slim/core/XmlMappingTest.java
@@ -12,11 +12,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.PropertyException;
 import javax.xml.bind.Unmarshaller;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
@@ -28,58 +31,69 @@ import org.junit.Test;
  *
  * @author Robert
  */
-public class XmlMappingTest extends BaseTest{
-    
-    private File mTestFileLoad;
-    private File mTestFileSave;
+public class XmlMappingTest extends BaseTest {
+
+    private File mEventTestFileWrite;
+    private File mUserTestFileWrite;
+    private File mEventTestFileRead;
+    private File mUserTestFileRead;
+
     private Marshaller mMarshaller;
     private Unmarshaller mUnmarshaller;
-    
-    //@Before
-    public void prepareXmlMarshalling() throws PropertyException, JAXBException{
-        mTestFileLoad = new File(XmlMappingTest.class.getResource("/robert.xml").getPath().replace("%20", " "));
-        mTestFileSave = new File(XmlMappingTest.class.getResource("/randomUser.xml").getPath().replace("%20", " "));
-        
-        JAXBContext context = JAXBContext.newInstance(User.class, Event.class);
-        
-       mMarshaller = context.createMarshaller();
-       mMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+    @Before
+    public void prepareXmlMarshalling() throws PropertyException {
+        mEventTestFileRead = new File(System.getProperty("user.dir") + "/src/test/java/resources/eventTestRead.xml");
+        mEventTestFileWrite = new File(System.getProperty("user.dir") + "/src/test/java/resources/eventTestWrite.xml");
+        mUserTestFileRead = new File(System.getProperty("user.dir") + "/src/test/java/resources/userTestRead.xml");
+        mEventTestFileWrite = new File(System.getProperty("user.dir") + "/src/test/java/resources/userTestWrite.xml");
+String message = "Test";
+        JAXBContext context;
+        try {
+            context = JAXBContext.newInstance(User.class, Event.class);
+             mMarshaller = context.createMarshaller();
+        mMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        mUnmarshaller = context.createUnmarshaller();
+        } catch (JAXBException ex) {
+            message = ex.getMessage();
+            Logger.getLogger(XmlMappingTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
        
-       mUnmarshaller = context.createUnmarshaller();
     }
     
-    //@Test
-    public void createValidXmlFile() throws JAXBException, IOException{
-        
-        if(mTestFileSave.exists())
-        {
-            mTestFileSave.delete();
+    @Test
+    public void createValidUserXmlFile() throws IOException, JAXBException{
+        if(mUserTestFileWrite.exists()){
+            mUserTestFileWrite.delete();
         }
         
-        /**
-         * Erzeugt zufälligen Nutzer
-         */   
-        User user = null;
+        //Create xml file for user
+        User user = new User("nickName", "firstName", "lastName", 0, "about", "imageurl");
+        mMarshaller.marshal(user, new FileWriter(mUserTestFileWrite));
         
-        //Serialisieren
-        mMarshaller.marshal(user, new FileWriter(mTestFileSave));
+        //Check if file was created
+        assertThat(mUserTestFileWrite.exists(), is(true));
+        assertThat(mUserTestFileWrite.isFile(), is(true));
         
-        assertThat(mTestFileSave.exists(), is(true));
-        assertThat(mTestFileSave.isFile(), is(true));
+        //deserialize
+        User desUser = (User) mUnmarshaller.unmarshal(mUserTestFileWrite);
         
-        //Deserialisieren
-        User desUser = (User) mUnmarshaller.unmarshal(mTestFileSave);
-        
+        //Check if everything worked
         assertThat(desUser, is(not(sameInstance(user))));
         assertThat(desUser.getmID(), is(user.getmID()));
+        assertThat(desUser.getmNickName(), equalTo(user.getmNickName()));
+        assertThat(desUser.getmFirstName(), equalTo(user.getmFirstName()));
+        assertThat(desUser.getmLastName(), equalTo(user.getmLastName()));
+        assertThat(desUser.getmAbout(), equalTo(user.getmAbout()));
+        assertThat(desUser.getmBirthday(), is(user.getmBirthday()));
+        
     }
-    
-   // @Test
-    public void loadFromXmlFile() throws JAXBException, FileNotFoundException{
-        User loadedUser = (User) mUnmarshaller.unmarshal(new FileReader(mTestFileLoad));
 
-        assertThat(loadedUser.getmFirstName(), is("Robert"));
-        assertThat(loadedUser.getmBirthday(), is("21.3.1993"));
+    // @Test
+    public void loadFromXmlFile() throws JAXBException, FileNotFoundException {
+       
     }
-    
+
 }
