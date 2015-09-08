67ed5cf65b2499e16065318c1794a678d130e13f
Robert Wolfinger <rwolfinger@cellular.de>
2015-09-08 15:49:56 +0200

    Continued work with rest module

---
+++ rest/src/main/java/slim/rest/SlimApplication.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slim.rest;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import slim.core.SlimService;
+import slim.core.impl.SlimServiceInMemory;
+
+/**
+ *
+ * @author Robert
+ */
+public class SlimApplication extends ResourceConfig{
+    
+    public SlimApplication(){
+        
+        register(new AbstractBinder() {
+
+            @Override
+            protected void configure() {
+                bind(new SlimServiceInMemory()).to(SlimService.class);
+            }
+        });
+    }
+}
---
+++ rest/src/test/java/slim/rest/RestBaseTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slim.rest;
+
+import java.io.File;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ *
+ * @author Robert
+ */
+public class RestBaseTest {
+     private static final String CLASS_NAME = BaseTest.class.getName();
+    SlimService mSlimService;
+    SlimDBServiceImpl mSlimDatabase;
+
+    /**
+     * Prepare the initial setup before testing
+     */
+    @Before
+    public void buildService() {
+        mSlimService = new SlimServiceInMemory();
+        mSlimDatabase = new SlimDBServiceImpl(Constants.MOCK_DB_NAME);
+        mSlimService.setDatabase(mSlimDatabase);
+    }
+
+    /**
+     * Tear down the created setup
+     */
+    @After
+    public void tearDownService() {
+        // Cleanup the mock database
+        mSlimDatabase.close();
+        File file = new File(System.getProperty("user.dir") + "/" + Constants.MOCK_DB_NAME);
+        if (file.exists()) {
+            file.delete();
+        } else {
+            System.err.println(CLASS_NAME + " could not delete mockup database. File does not exist!");
+        }
+    }
+
+    protected String getRandomName() {
+        return RandomStringUtils.randomAlphanumeric(RandomUtils.nextInt(10) + 1);
+    }
+
+    protected String getRandomUUIDString() {
+        return String.valueOf(UUID.randomUUID());
+    }
+
+    protected String getRandomDateString() {
+        Date date = new Date(Math.abs(System.currentTimeMillis() - RandomUtils.nextLong()));
+        return date.toString();
+    }
