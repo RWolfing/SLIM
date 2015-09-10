066bea7c795eb38c32c29c59800eb460e9844eb1
Robert Wolfinger <rwolfinger@cellular.de>
2015-09-10 12:35:10 +0200

    Created resource tests

--- rest/src/test/java/slim/rest/RestBaseTest.java
+++ rest/src/test/java/slim/rest/RestBaseTest.java
@@ -6,17 +6,36 @@
 package slim.rest;
 
 import java.io.File;
-import java.util.Date;
-import java.util.UUID;
+import javax.ws.rs.core.Context;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.math.RandomUtils;
+import org.junit.After;
+import org.junit.Before;
+import slim.core.SlimService;
+import slim.core.impl.SlimDBServiceImpl;
+import slim.core.impl.SlimServiceInMemory;
+import slim.core.model.Event;
+import slim.core.model.Location;
+import slim.core.model.User;
+import slim.core.utils.Constants;
 
 /**
  *
  * @author Robert
  */
-public class RestBaseTest {
-     private static final String CLASS_NAME = BaseTest.class.getName();
-    SlimService mSlimService;
-    SlimDBServiceImpl mSlimDatabase;
+public abstract class RestBaseTest {
+
+    private static final String CLASS_NAME = RestBaseTest.class.getName();
+    protected static final int SUM_USERS = 5;
+    protected static final int SUM_EVENTS = 3;
+
+    @Context
+    protected SlimService mSlimService;
+    private SlimDBServiceImpl mSlimDatabase;
+
+    protected User mTestUser;
+    protected User mPartyFriend1;
+    protected User mPartyFriend2;
 
     /**
      * Prepare the initial setup before testing
@@ -26,6 +45,41 @@ public class RestBaseTest {
         mSlimService = new SlimServiceInMemory();
         mSlimDatabase = new SlimDBServiceImpl(Constants.MOCK_DB_NAME);
         mSlimService.setDatabase(mSlimDatabase);
+
+        User max = new User("Partyloewe", "Max", "Mustermann", 500000, "Spiel mit mir", "www.bilder.de/bild1");
+        User anna = new User("Partyloewin", "Anna", "Branco", 600000, "Spiel mit mir", "www.bilder.de/bild3");
+        User klaus = new User("klaus", "klaus", "schulz", 5000000, "Spiel mit mir", "www.bilder.de/bild4");
+        User gustav = new User("gmueller", "Gustav", "Mueller", 1500000, "Spiel mit mir", "www.bilder.de/bild5");
+        User bianca = new User("bschaber", "Bianca", "Schaber", 53420000, "Spiel mit mir", "www.bilder.de/bild6");
+
+        mTestUser = max = mSlimDatabase.createUser(max);
+        mPartyFriend1 = anna = mSlimDatabase.createUser(anna);
+        klaus = mSlimDatabase.createUser(klaus);
+        mPartyFriend2 = gustav = mSlimDatabase.createUser(gustav);
+        bianca = mSlimDatabase.createUser(bianca);
+
+        Location location0 = new Location(RandomUtils.nextLong(), RandomUtils.nextLong());
+        Location location1 = new Location(RandomUtils.nextLong(), RandomUtils.nextLong());
+        Location location2 = new Location(RandomUtils.nextLong(), RandomUtils.nextLong());
+
+        location0 = mSlimDatabase.createLocation(location0);
+        location1 = mSlimDatabase.createLocation(location1);
+        location2 = mSlimDatabase.createLocation(location2);
+
+        Event event0 = new Event("Event0", location0, 8000, 90000, "Event 0 ist toll", max);
+        event0.addGuest(gustav);
+        event0.addGuest(anna);
+        mSlimDatabase.createEvent(event0);
+
+        Event event1 = new Event("Event1", location1, 123100, 9000000, "Event 1 ist toll", anna);
+        event1.addGuest(gustav);
+        mSlimDatabase.createEvent(event1);
+
+        Event event2 = new Event("Event2", location2, 654616, 78951651, "Event 2 ist toll", bianca);
+        event2.addGuest(klaus);
+        event2.addGuest(max);
+        event2.addGuest(gustav);
+        mSlimDatabase.createEvent(event2);
     }
 
     /**
@@ -47,11 +101,7 @@ public class RestBaseTest {
         return RandomStringUtils.randomAlphanumeric(RandomUtils.nextInt(10) + 1);
     }
 
-    protected String getRandomUUIDString() {
-        return String.valueOf(UUID.randomUUID());
+    public SlimService getSlimService() {
+        return mSlimService;
     }
-
-    protected String getRandomDateString() {
-        Date date = new Date(Math.abs(System.currentTimeMillis() - RandomUtils.nextLong()));
-        return date.toString();
-    }
+}
--- rest/src/test/java/slim/rest/UserResourceImplTest.java
+++ rest/src/test/java/slim/rest/UserResourceImplTest.java
@@ -5,10 +5,141 @@
  */
 package slim.rest;
 
+import java.util.List;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Before;
+import slim.rest.impl.UserResourceImpl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+import slim.core.model.User;
+
 /**
  *
  * @author Robert
  */
-public class UserResourceImplTest {
-    
+public class UserResourceImplTest extends RestBaseTest {
+
+    private UserResourceImpl mUserResource;
+    private UriInfo mUriInfo;
+
+    @Before
+    public void prepareResourcesTest() {
+        mUserResource = new UserResourceImpl();
+        mUserResource.setmSlimService(mSlimService);
+    }
+
+    @Before
+    public void mockUriInfo() {
+        mUriInfo = mock(UriInfo.class);
+        final UriBuilder fromResource = UriBuilder.fromResource(UserResourceImpl.class);
+        when(mUriInfo.getAbsolutePathBuilder()).thenAnswer(new Answer<UriBuilder>() {
+
+            @Override
+            public UriBuilder answer(InvocationOnMock invocation) throws Throwable {
+                return fromResource;
+            }
+        });
+    }
+
+    @Test
+    public void createUserSuccess() {
+        Response response = mUserResource.createUser(mUriInfo, "TestNickname", "TestFirstName", "TestLastName", 500000, "TestDescription", "www.test.de");
+        assertThat(response.getStatus(), is(Response.Status.CREATED.getStatusCode()));
+
+        User user = (User) response.getEntity();
+        response = mUserResource.fetchUserById(user.getmID());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        assertThat(user.getmID(), is(((User) response.getEntity()).getmID()));
+    }
+
+    @Test
+    public void createUserFails() {
+        Response response = mUserResource.createUser(mUriInfo, null, null, null, 500000, "TestDescription", "www.test.de");
+        assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void fetchUserById() {
+        //Existing
+        Response response = mUserResource.fetchUserById(mTestUser.getmID());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        assertThat(mTestUser.getmID(), is(((User) response.getEntity()).getmID()));
+
+        //Bad id
+        response = mUserResource.fetchUserById(-10);
+        assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+
+        //Not existing user
+        response = mUserResource.fetchUserById(100);
+        assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    public void fetchAllUsers() {
+        Response response = mUserResource.fetchAllUsers();
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        List<User> result = (List<User>) response.getEntity();
+        assertThat(result.size(), is(SUM_USERS));
+    }
+
+    @Test
+    public void updateUser() {
+        //Test success
+        Response response = mUserResource.updateUser(mTestUser.getmID(), "maxi", mTestUser.getmBirthday(), "new description", mTestUser.getmImageUrl());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+
+        User user = (User) response.getEntity();
+        assertThat(user.getmID(), is(mTestUser.getmID()));
+        assertThat(user.getmNickName(), is("maxi"));
+        assertThat(user.getmAbout(), is("new description"));
+
+        //Test failure bad id
+        response = mUserResource.updateUser(-100, "maxi", mTestUser.getmBirthday(), "new description", mTestUser.getmImageUrl());
+        assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+
+        //Test failure not found
+        response = mUserResource.updateUser(100, "maxi", mTestUser.getmBirthday(), "new description", mTestUser.getmImageUrl());
+        assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    public void doesHePartyWithMe() {
+        //Anna parties with gustav
+        Response response = mUserResource.doesHePartyWithMe(mPartyFriend1.getmID(), mPartyFriend2.getmID());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        assertThat(response.getEntity(), is(true));
+        
+        //Anna does not party with max
+        response = mUserResource.doesHePartyWithMe(mPartyFriend1.getmID(), mTestUser.getmID());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        assertThat(response.getEntity(), is(false));
+        
+        //Failure invalid ids
+        response = mUserResource.doesHePartyWithMe(-100, mTestUser.getmID());
+        assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void deleteUser() {
+        //Test success
+        Response response = mUserResource.deleteUser(mTestUser.getmID());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+
+        //Test bad id
+        response = mUserResource.deleteUser(-100);
+        assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+
+        //Test not existing
+        response = mUserResource.deleteUser(mTestUser.getmID());
+        assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
 }
