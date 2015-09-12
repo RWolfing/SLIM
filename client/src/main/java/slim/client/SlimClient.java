7e66c238a672566ef25b3fecd0832a84d3de684b
Robert Wolfinger <rwolfinger@cellular.de>
2015-09-12 16:23:43 +0200

    Added Main class and reworked SlimClient

--- client/src/main/java/slim/client/SlimClient.java
+++ client/src/main/java/slim/client/SlimClient.java
@@ -1,43 +1,128 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package slim.client;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Scanner;
 import slim.client.services.EventService;
 import slim.client.services.LocationService;
 import slim.client.services.SlimService;
 import slim.client.services.UserService;
 import slim.core.model.Event;
+import slim.core.model.Location;
 import slim.core.model.User;
 
 /**
+ * Client to test the restful slim webservice
  *
- * @author Robert
+ * @author Robert Wolfinger
  */
 public class SlimClient {
 
     private static final String serviceBaseURI = "http://localhost:8080";
 
-    private static UserService mUserService = new UserService(serviceBaseURI, SlimService.MediaType.JSON);
-    private static EventService mEventService = new EventService(serviceBaseURI, SlimService.MediaType.JSON);
-    private static LocationService mLocationService = new LocationService(serviceBaseURI, SlimService.MediaType.JSON);
+    private UserService mUserService = new UserService(serviceBaseURI, SlimService.MediaType.JSON);
+    private EventService mEventService = new EventService(serviceBaseURI, SlimService.MediaType.JSON);
+    private LocationService mLocationService = new LocationService(serviceBaseURI, SlimService.MediaType.JSON);
 
-    public static void main(String[] args) {
-        testUserFunctionality();
-        testEventFunctionality();
+    private Calendar mCalendar = Calendar.getInstance();
+    private User mUserMaxMuster;
+    private User mUserABranco;
+    private User mUserLKopfer;
+    private User mUserMMueller;
+    private User mUserGSchulze;
+
+    private Event mEventStandard;
+    private Event mEventWithGuests;
+
+    private Location mLocationSingle;
+    private Location mLocationWithEvents;
+
+    private Scanner mScanner;
+    private BufferedReader mReader;
+
+    public SlimClient() {
+        mScanner = new Scanner(System.in);
     }
 
-    private static void testUserFunctionality() {
+    public void start() {
+        boolean nextRound = true;
+        while (nextRound) {
+            showAvailableOptions();
+            int input = mScanner.nextInt();
+            switch (input) {
+                case 1:
+                    testUserFunctionality();
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("No valid option!");
+                    nextRound = false;
+            }
+        }
+
+    }
+
+    public void cleanupDatabase() {
+        File coreModul = new File(System.getProperty("user.dir"));
+        File dbFile = new File(coreModul.getParent() + "/slim.db");
+        if (dbFile.exists()) {
+            if (dbFile.delete()) {
+                System.out.println("Deleted database! " + dbFile.getAbsolutePath());
+            } else {
+                System.err.println("Could not delete database! " + dbFile.getAbsolutePath());
+            }
+        }
+    }
+
+    public void showAvailableOptions() {
+        System.out.println("|------- Available options --------|");
+        System.out.println("| 1: Show User create->update->retrieve->delete->retrieve |");
+    }
+
+    public boolean setupDataPool() {
+        System.out.println("Setting up data pool.....");
+        //Setup users
+        mCalendar.set(2000, 3, 20);
+        mUserMaxMuster = mUserService.createUser("mmuster", "Max", "Mustermann", mCalendar.getTimeInMillis(), "Ich bin der Max!", "www.meine-bilder.de").getResultContent();
+        mCalendar.set(1955, 5, 10);
+        mUserABranco = mUserService.createUser("abranco", "Anne", "Branco", mCalendar.getTimeInMillis(), "Ich bin die Anne!", "www.meine-bilder.de").getResultContent();
+        mCalendar.set(1970, 7, 1);
+        mUserLKopfer = mUserService.createUser("lkopfer", "Lasse", "kopfer", mCalendar.getTimeInMillis(), "Ich bin der Lasse!", "www.meine-bilder.de").getResultContent();
+        mCalendar.set(1988, 2, 15);
+        mUserMMueller = mUserService.createUser("mmueller", "Marvin", "Mueller", mCalendar.getTimeInMillis(), "Ich bin der Marvin!", "www.meine-bilder.de").getResultContent();
+        mCalendar.set(1995, 9, 25);
+        mUserGSchulze = mUserService.createUser("gschulze", "Guenther", "Schulze", mCalendar.getTimeInMillis(), "Ich bin der Guenther!", "www.meine-bilder.de").getResultContent();
+
+        mLocationSingle = mLocationService.createLocation("Single Location", 1591050909l, 4091509090l).getResultContent();
+        mLocationWithEvents = mLocationService.createLocation("Event Location", 4091509090l, -490815090l).getResultContent();
+
+        //Setup events
+        mEventStandard = mEventService.createEvent("Standard Event", 4091509090l, -490815090l, 98795161l, 121059019100l, "Das ist ein Standard event", mUserMaxMuster.getmID()).getResultContent();
+        mEventWithGuests = mEventService.createEvent("Guest Event", 4091509090l, -490815090l, 98795161l, 121059019100l, "Das ist ein Event mit Gaesten", mUserMaxMuster.getmID()).getResultContent();
+        mEventService.addGuestToEvent(mEventWithGuests.getmID(), mUserABranco.getmID());
+        mEventService.addGuestToEvent(mEventWithGuests.getmID(), mUserLKopfer.getmID());
+        mEventService.addGuestToEvent(mEventWithGuests.getmID(), mUserMMueller.getmID());
+
+        if (mUserABranco != null && mUserGSchulze != null && mUserLKopfer != null && mUserMMueller != null && mUserMaxMuster != null && mEventStandard != null && mEventWithGuests != null && mLocationSingle != null && mLocationWithEvents != null) {
+            System.out.println("Setting up data pool succeeded!");
+            return true;
+        } else {
+            System.err.println("Setting up data pool did NOT succeed!");
+            System.err.println("The service can not be tested...");
+            System.err.println("Please start the server & delete the database file!");
+            return false;
+        }
+    }
+
+    public void testUserFunctionality() {
         System.out.println();
         System.out.println("---- CREATING USER ----");
 
         Calendar cal = Calendar.getInstance();
         cal.set(2000, 3, 20);
-        User userMax = mUserService.createUser("mmuster", "Max", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getmResultContent();
+        User userMax = mUserService.createUser("mmuster", "Max", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getResultContent();
 
         System.out.println();
         System.out.println("---- UPDATING USER ----");
@@ -54,18 +139,18 @@ public class SlimClient {
         System.out.println();
         System.out.println("---- RETRIEVING USER ----");
         mUserService.fetchUserById(userMax.getmID());
-        
-        userMax = mUserService.createUser("mmuster", "Max", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getmResultContent();
-        userMax = mUserService.createUser("mmuster", "Max1", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getmResultContent();
-        userMax = mUserService.createUser("mmuster", "Max2", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getmResultContent();
-        
+
+        userMax = mUserService.createUser("mmuster", "Max", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getResultContent();
+        userMax = mUserService.createUser("mmuster", "Max1", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getResultContent();
+        userMax = mUserService.createUser("mmuster", "Max2", "Mustermann", cal.getTimeInMillis(), "Super läufts", "www.geilesaeue.de").getResultContent();
+
         mUserService.fetchAllUsers();
     }
-    
-    private static void testEventFunctionality(){
-        User userMax = mUserService.createUser("mmuster", "Max", "Mustermann", 5910591000l, "Super läufts", "www.geilesaeue.de").getmResultContent();
-        Event event = mEventService.createEvent("test", 5000, 5000, 500, 5000, "description", userMax.getmID()).getmResultContent();
-        
-        List<Event> events = mEventService.fetchAllEvents().getmResultContent();
+
+    public void testEventFunctionality() {
+        User userMax = mUserService.createUser("mmuster", "Max", "Mustermann", 5910591000l, "Super läufts", "www.geilesaeue.de").getResultContent();
+        Event event = mEventService.createEvent("test", 5000, 5000, 500, 5000, "description", userMax.getmID()).getResultContent();
+
+        List<Event> events = mEventService.fetchAllEvents().getResultContent();
     }
 }
